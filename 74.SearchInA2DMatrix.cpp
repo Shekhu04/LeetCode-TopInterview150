a278629bbd159fa4557b1ad7ae68b029cef4f6d8
Shikhar Gupta <107641403+Shekhu04@users.noreply.github.com>
2024-01-09 22:53:47 +0530

    Create 74.SearchInA2DMatrix.cpp

---
+++ 74.SearchInA2DMatrix.cpp
@@ -0,0 +1,49 @@
+/*bool binarySearch(vector<int>&nums, int target){
+    int n = nums.size();
+    int low=0, high = n-1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target) return true;
+        else if(target > nums[mid]) low=mid+1;
+        else high = mid-1;
+    }
+    return false;
+}*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+          int n = matrix.size(); //size of row
+          int m = matrix[0].size(); //size of column
+
+      /*BFA
+      //traverse the matrix
+      for(int i=0; i<n; i++){
+          for(int j=0; j<m; j++){
+              if(matrix[i][j] == target) return true;
+          }
+      } 
+      return false;*/
+
+     /*Better Approach
+      for (int i = 0; i < n; i++) {
+        if (matrix[i][0] <= target && target <= matrix[i][m - 1]) {
+            return binarySearch(matrix[i], target);
+        }
+    }
+    return false;
+    }*/
+
+    //Optimal Approach
+     int low = 0, high = n * m - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int row = mid / m, col = mid % m;
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+    }
+};
